71d8107bad662632d0fbcb5cb186b3dd6507aba7
wen931130 <150638551+wen931130@users.noreply.github.com>
2024-05-27 10:46:45 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,6 +66,27 @@ function drawSkeleton() {  //偵測出各點並畫出線條
         line(partA.x, partA.y, partB.x, partB.y);
       }
     }
+    //eye
+    partL = pose.keypoints[1];
+    partR = pose.keypoints[2];
+    if(partL.score > 0.1)
+    {
+      ellipse(partL.x,partL.y,50)
+    }
+
+    if(partR.score > 0.1)
+    {
+      ellipse(partR.x,partR.y,50)
+    }
+    //
+    partA = pose.keypoint[0];
+    if(partA.score > 0.1)
+    {
+      fill("ff0000")
+      ellipse(partA.x,partA.y,50)
+      fill("#fffff")
+    }
+
     // shoulder to shoulder
     partA = pose.keypoints[5];
     partB = pose.keypoints[6];
